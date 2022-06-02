b42c9cfc6dab166c3b98a78192605c35e3db85ef
dishakathane <dishakathane1995@gmail.com>
2022-06-02 15:21:06 +0530

    after completing 4th problem

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const _ = require("lodash");
 const logger = require('../logger/logger.js');
 
 const helper = require('../Util/helper.js');
@@ -16,7 +17,30 @@ router.get('/test-me', function(req, res){
     formatter.upperCase();
 
     res.send("Api works successfully, Check your terminal")
-})
+});
+ 
+router.get('/hello', function(req,res){
+  let allMonths =['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  console.log("chunk in 4 equal part is:- ",_.chunk(allMonths,3 ));// Making chunks of size 3
+  
+  let arrOfOddNum =[1,3,5,7,9,11,13,15,17,19];
+  console.log("By Using tail Function Last 9 ELements is :- ", _.tail(arrOfOddNum));
+
+  let getUnion = _.union([1,2,3],[2,3,4],[3,9,5],[4,5,6],[5,8,7]);
+  console.log("thi unique value returns lodash union:-" ,getUnion);
+  
+  let movies =[
+      ["horror","The Shining"],
+      ["drama","Titanic"],
+      ["thriller","Shutter Island"],
+      ["fantasy","Pans Labyrinth"]
+    ]
+  let objForm= _.fromPairs(movies);
+  
+console.log(objForm)
+
+  res.send("Done! Successfully")
+});
 
 
 
