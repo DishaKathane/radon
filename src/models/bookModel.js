54d4a6b78b53807bbfa57519bfde5755b373b440
dishakathane <dishakathane1995@gmail.com>
2022-06-09 22:11:17 +0530

    assignment 9june

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -3,15 +3,25 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
     name: String,
-    author_id: {
+    author: {
         type: ObjectId,
-        ref: "AuthorPop"
+        ref: "AuthorPopjune"
     },
     price: Number,
-    ratings: Number
+    ratings: Number,
+    publisher:{
+        type: ObjectId,
+        ref: "PublisherPopjune"
+    },
+
+    isHardCover :{
+        type:Boolean,
+        default:false
+    }
+
 
 
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema )
+module.exports = mongoose.model('newBook', bookSchema )
